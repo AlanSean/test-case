625e7ef79f187eba7eed2eec0f68518de581cfb0
Mr.huang <2316694914@qq.com>
2018-08-31 13:44:05 +0800

    test-case

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+var assert = require("assert");
+
+describe("闭包应用",()=>{
+    it("a===0",()=>{
+         assert.equal(0, 0)
+    })
+    it("方法引用",()=>{
+         assert.equal(0, 0)
+    })
+})
